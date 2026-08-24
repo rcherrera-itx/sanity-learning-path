73b93bd0c0c3af84cb6d779c9e2d7a7eb5c225df
Rafael Contreras Herrera <rcherrera@itx.com>
2026-08-24 09:14:30 -0600

    Configuration of Sanity Fecth and Sanity Live in Next.

---
+++ apps/web/src/sanity/lib/live.ts
@@ -0,0 +1,12 @@
+import { defineLive } from 'next-sanity/live'
+
+import { client } from './client'
+
+export const {
+    sanityFetch,
+    SanityLive
+} = defineLive({
+    client,
+    serverToken: false,
+    browserToken: false
+})
