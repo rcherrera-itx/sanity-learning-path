348fde14997c37d0be6a575c0507deb895c9d2a6
Rafael Contreras Herrera <rcherrera@itx.com>
2026-08-24 12:08:35 -0600

    Created resolve.ts for MainDocuments and Locations in Studio.

---
+++ apps/studio/src/presentation/resolve.ts
@@ -0,0 +1,53 @@
+import {
+    defineDocuments,
+    defineLocations,
+    type PresentationPluginOptions
+} from 'sanity/presentation';
+
+const mainDocuments = defineDocuments([
+    {
+        route: '/products/:slug',
+        filter: '_type == "product" && slug.current == $slug',
+    },
+    {
+        route: ':slug',
+        filter: '_type == "page" && slug.current == $slug',
+    }
+])
+
+const locations = {
+    product: defineLocations({
+        select: {
+            title: 'title',
+            slug: 'slug.current'
+        },
+        resolve: (document) => ({
+            locations: document?.slug
+                ? [
+                    {
+                        title: document.title || 'Product with no title',
+                        href: `/products/${document.slug}`
+                    }
+                ]
+                : [],
+        }),
+    }),
+    page: defineLocations({
+        select: {
+            title: 'title',
+            slug: 'slug.current'
+        },
+        resolve: (document) => ({
+            locations: document?.slug
+            ? [{
+                title: document.title || 'Page without title',
+                href: `/${document.slug}`
+            }] : [],
+        }),
+    }),
+}
+
+export const resolve: PresentationPluginOptions['resolve'] = {
+    mainDocuments,
+    locations
+}
