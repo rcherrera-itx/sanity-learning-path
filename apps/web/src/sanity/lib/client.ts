ef638102c9f91a816afebe57aea1990e463b067f
Rafael Contreras Herrera <rcherrera@itx.com>
2026-08-24 12:59:59 -0600

    Configured NEXT_PUBLIC_SANITY_STUDIO_URL in client.ts at Next.

--- apps/web/src/sanity/lib/client.ts
+++ apps/web/src/sanity/lib/client.ts
@@ -1,11 +1,18 @@
 // Client configuration that uses api.ts values
-
 import { createClient } from "next-sanity";
-import { apiVersion, dataset, projectId } from './api';
+import { 
+    apiVersion, 
+    dataset, 
+    projectId,
+    studioUrl
+} from './api';
 
 export const client = createClient({
     apiVersion,
     dataset,
     projectId,
-    useCdn: true
+    useCdn: true,
+    stega: {
+        studioUrl
+    }
 })
