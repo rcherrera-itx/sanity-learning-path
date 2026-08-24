8a6a96bdb7b9537141d40c2c5e1dac9de5241d54
Rafael Contreras Herrera <rcherrera@itx.com>
2026-08-24 13:49:47 -0600

    Disabled autoUpdated in sanity.cli.ts until appId is created at Studio.

--- apps/studio/sanity.cli.ts
+++ apps/studio/sanity.cli.ts
@@ -10,7 +10,7 @@ export default defineCliConfig({
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/cli#auto-updates
      */
-    autoUpdates: true,
+    autoUpdates: false,
   },
   typegen: {
     path: './queries/**/*.{ts,tsx}',
