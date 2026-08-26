0aac6fc83cd673e1f34fa3d5a248326e29212638
Rafael Contreras Herrera <rcherrera@itx.com>
2026-08-26 09:57:38 -0600

    Refactor for disable-draft-mode.tsx component to force full reload navigation when disabling Draft Mode.

--- apps/web/src/app/components/disable-draft-mode.tsx
+++ apps/web/src/app/components/disable-draft-mode.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useIsPresentationTool } from 'next-sanity/hooks';
-import Link from 'next/link';
 
 export function DisableDraftMode() {
     const isPresentationTool = useIsPresentationTool();
@@ -13,9 +12,9 @@ export function DisableDraftMode() {
     return (
         <aside className='fixed bottom-4 right-4 z-50 flex items-center gap-3 rounded-full bg-black px-4 py-2 text-sm text-white shadow-lg'>
             <span>Active Draft Mode</span>
-
-            {/* <a href="/api/draft-mode/disable" className='underline'>Deactivate</a> */}
-            <Link href="/api/draft-mode/disable" className='underline'>Deactivate</Link>
+            {/* Full document navigation required after clearing Draft Mode cookie */}
+            {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
+            <a href="/api/draft-mode/disable" className='underline'>Deactivate</a>
         </aside>
     )
 }
