386229d8c539f48bb80c84acf63a00ad9652dcac
Rafael Contreras Herrera <rcherrera@itx.com>
2026-08-26 09:57:02 -0600

    Refactor for disabling Draft cookie in api/draft-mode/disable.tsx at Web.

--- apps/web/src/app/api/draft-mode/disable/route.ts
+++ apps/web/src/app/api/draft-mode/disable/route.ts
@@ -1,10 +1,28 @@
+import { perspectiveCookieName } from '@sanity/preview-url-secret/constants';
 import { draftMode } from 'next/headers';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
     ; (await draftMode()).disable();
 
-    return NextResponse.redirect(
+    const response = NextResponse.redirect(
         new URL('/', request.url)
     )
+
+    const expired = [
+        `${perspectiveCookieName}=`,
+        'Path=/',
+        'HttpOnly',
+        'Secure',
+        'SameSite=None',
+        'Max-Age=0'
+    ];
+
+    response.headers.append('Set-Cookie', expired.join('; '));
+    response.headers.append(
+        'Set-Cookie',
+        [...expired, 'Partitioned'].join('; ')
+    );
+
+    return response;
 }
