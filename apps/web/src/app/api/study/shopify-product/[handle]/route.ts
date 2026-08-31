00ba81ab22fd0cdd33da2c14fbb4b2f909c3f25e
Rafael Contreras Herrera <rcherrera@itx.com>
2026-08-30 22:17:44 -0600

    Refactor for Shopify Product Route test.

--- apps/web/src/app/api/study/shopify-product/[handle]/route.ts
+++ apps/web/src/app/api/study/shopify-product/[handle]/route.ts
@@ -1,19 +1,48 @@
-import { getProductByHandle } from '@/shopify/queries/product';
 import { NextResponse } from 'next/server';
+import { getProductByHandle } from '@/shopify/queries/product';
 
-export async function GET(params: Promise<{
-    handle: string
-}>) {
-    const { handle } = await params;
-    const product = await getProductByHandle(handle);
+type ShopifyProductRouteContext = {
+    params: Promise<{
+        handle: string
+    }>;
+};
 
-    if(product) {
-        return 200;
+export async function GET(
+    _request: Request,
+    { params }: ShopifyProductRouteContext
+) {
+    try {
+        const { handle } = await params;
+        const product = await getProductByHandle(handle);
+
+        if (!product) {
+            return NextResponse.json(
+                {
+                    message: "Shopify product not found!",
+                },
+                {
+                    status: 404
+                }
+            );
+        }
+
+        return NextResponse.json(
+            { product },
+            { status: 200 }
+        );
+    } catch (error: unknown) {
+        const message = error instanceof Error
+            ? error.message : "Unkown message";
+
+        console.error("[SHOPIFY][PRODUCT_BY_HANDLE]", { message });
+
+        return NextResponse.json(
+            {
+                message: "Shopify product request failed!",
+            },
+            {
+                status: 500
+            }
+        );
     }
-
-    if(product === null) {
-        return 400;
-    }
-
-    return 500;
 }
